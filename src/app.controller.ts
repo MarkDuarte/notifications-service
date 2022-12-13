da0fd4647f03c8edb06f100f5c9f4e3e23342009
Marcos Duarte <marcosduarte1994@gmail.com>
2022-12-12 23:01:05 -0300

    feat: created route post create notiications

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,4 +1,5 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Post } from '@nestjs/common';
+import { randomUUID } from 'crypto';
 import { PrismaService } from './prisma.service';
 
 @Controller('notifications')
@@ -6,7 +7,19 @@ export class AppController {
   constructor(private readonly prisma: PrismaService) {}
 
   @Get()
-  getHello() {
+  list() {
     return this.prisma.notification.findMany();
+  }
+
+  @Post()
+  async reate() {
+    await this.prisma.notification.create({
+      data: {
+        id: randomUUID(),
+        content: 'Voce tem uma nova solicitacao de amizade',
+        category: 'social',
+        recipientId: randomUUID(),
+      },
+    });
   }
 }
