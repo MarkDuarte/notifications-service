25e81522b37ebb71764466345887cab71808f2c4
Marcos Duarte <atendimento5@cubosis.com.br>
2022-12-14 11:38:32 -0300

    feat: struct DTO in project

--- src/application/entities/content.ts
+++ src/application/entities/content.ts
@@ -13,8 +13,9 @@ export class Content {
     const isContentLengthValid = this.validateContentLength(content);
 
     if (!isContentLengthValid) {
-      throw new Error(`Content length error.`);
+      throw new Error('Content length error.');
     }
+
     this.content = content;
   }
 }
---
+++ src/infra/http/http.module.ts
@@ -0,0 +1,11 @@
+import { Module } from '@nestjs/common';
+import { SendNotification } from 'src/application/use-cases/send-notification';
+import { DatabaseModule } from '../database/database.module';
+import { NotifcationsController } from './controllers/notifications.controller';
+
+@Module({
+  imports: [DatabaseModule],
+  controllers: [NotifcationsController],
+  providers: [SendNotification],
+})
+export class HttpModule {}
