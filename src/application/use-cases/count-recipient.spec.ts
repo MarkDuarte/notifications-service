23fc14583f2872fc4d0db634c074db117cad8c87
Marcos Duarte <atendimento5@cubosis.com.br>
2022-12-16 15:58:54 -0300

    feat: create method findBy to locate the notifications

---
+++ src/application/use-cases/count-recipient.spec.ts
@@ -0,0 +1,43 @@
+import { Content } from '@application/entities/content';
+import { Notification } from '@application/entities/notification';
+import { InMemoryNotificationsRepository } from '@test/repositories/in-memory-notifications-repository';
+import { CountRecipientNotification } from './count-recipient-notification';
+
+describe('Count recipients notifications', () => {
+  it('should be able to count recipient notification', async () => {
+    const notificationsRepository = new InMemoryNotificationsRepository();
+    const countRecipientNotification = new CountRecipientNotification(
+      notificationsRepository,
+    );
+
+    notificationsRepository.create(
+      new Notification({
+        category: 'social',
+        content: new Content('Nova solicitação de amizade'),
+        recipientId: 'recipient-1',
+      }),
+    );
+
+    notificationsRepository.create(
+      new Notification({
+        category: 'social',
+        content: new Content('Nova solicitação de amizade'),
+        recipientId: 'recipient-1',
+      }),
+    );
+
+    notificationsRepository.create(
+      new Notification({
+        category: 'social',
+        content: new Content('Nova solicitação de amizade'),
+        recipientId: 'recipient-2',
+      }),
+    );
+
+    const { count } = await countRecipientNotification.execute({
+      recipientId: 'recipient-1',
+    });
+
+    expect(count).toEqual(2);
+  });
+});
